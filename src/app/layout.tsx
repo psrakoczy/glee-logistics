cf2b04ac6e81b351afa0133f5f6e5233f9a6ad9d
psrakoczy <psrakoczy@outlook.com>
2026-03-26 12:19:46 +0100

    Block search engine indexing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,10 @@
 export const metadata = {
   title: "GLEE Logistics GmbH",
   description: "Logistics, transportation, HR outsourcing and automotive services in Germany.",
+  robots: {
+    index: false,
+    follow: false,
+  },
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
