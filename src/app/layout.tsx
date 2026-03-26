8589a973fb0af6a779dabc1b2beff89beab24521
psrakoczy <psrakoczy@outlook.com>
2026-03-26 12:51:48 +0100

    Add favicon concept D

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,9 @@ export const metadata = {
     index: false,
     follow: false,
   },
+  icons: {
+    icon: "/favicon.svg",
+  },
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
